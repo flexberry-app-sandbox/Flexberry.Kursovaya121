ea9c9bfc3357799f1fb2731834e2d242e9d10160
Flexberry-man <mail@flexberry.net>
2023-12-12 05:12:42 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -22,31 +22,30 @@ export default Controller.extend({
             link: 'i-i-s-kursovaya12-дир-отд-по-раб-с-кл-l',
             caption: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-дир-отд-по-раб-с-кл-l.caption'),
             title: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-дир-отд-по-раб-с-кл-l.title'),
-            icon: 'calendar',
+            icon: 'file',
             children: null
           }, {
             link: 'i-i-s-kursovaya12-расчет-ост-врем-l',
             caption: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-расчет-ост-врем-l.caption'),
             title: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-расчет-ост-врем-l.title'),
-            icon: 'file',
+            icon: 'archive',
             children: null
           }, {
             link: 'i-i-s-kursovaya12-заключ-договора-l',
             caption: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-заключ-договора-l.caption'),
             title: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-заключ-договора-l.title'),
-            icon: 'list',
+            icon: 'folder open',
             children: null
           }, {
             link: 'i-i-s-kursovaya12-клиент-l',
             caption: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-клиент-l.caption'),
             title: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-клиент-l.title'),
-            icon: 'paperclip',
+            icon: 'briefcase',
             children: null
           }, {
             link: 'i-i-s-kursovaya12-расторж-договора-l',
             caption: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-расторж-договора-l.caption'),
             title: i18n.t('forms.application.sitemap.kursovaya12.i-i-s-kursovaya12-расторж-договора-l.title'),
-            icon: 'suitcase',
             children: null
           }]
         }
